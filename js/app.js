4170a5a731cc6596522452239d9b78a134fe4b5c
Niels Schroyen <nielsschroye@gmail.com>
2014-03-11 18:44:29 +0100

    playing with github api

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 function authorize()
 {
-	$.get('https://github.com/login/oauth/authorize', function(x){
-	alert(x);	  });
+	var github = new Github({
+  username: "nielsschroyen",
+  password: "Hivvd13d",
+  auth: "basic"
+		});
+		
+		
+		
+		var repo = github.getRepo("nielsschroyen", "nielsschroyen.github.io");
+		
+		repo.read('master', 'model/model.json', function(err, data) {
+		 alert(data);
+		});
+		
+		repo.getTree('master?recursive=true', function(err, tree) {
+		
+		alert(tree);
+		});
+		
+		repo.show(function(err, repo) {
+		alert(repo);
+		});
 	
 	return false;
 }
